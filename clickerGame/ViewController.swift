0f2f981b8b15904341d3a3844e0067b10087191f
Tyler Mok <tymok06@gmail.com>
2023-10-03 09:04:38 -0500

    Initial Commit

---
+++ clickerGame/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  clickerGame
+//
+//  Created by TYLER MOK on 10/3/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
