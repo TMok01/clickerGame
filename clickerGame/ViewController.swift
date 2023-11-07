e7aee7f7156819350f4d35f196fc60fca16a3fb7
Tyler Mok <tymok06@gmail.com>
2023-11-07 09:41:45 -0600

    clickerGame1

--- clickerGame/ViewController.swift
+++ clickerGame/ViewController.swift
@@ -9,11 +9,36 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var pointsOutlet: UILabel!
+    
+    var storePoints = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
 
+    
+    @IBAction func invaderButton(_ sender: Any) {
+        var points = Int(pointsOutlet.text!) ?? 0
+        points += 1
+        storePoints = points
+        pointsOutlet.text = "\(points)"
+    }
+    
+    
+    
+    @IBAction func storeButton(_ sender: Any) {
+        performSegue(withIdentifier: "blah", sender: self)
+        print(storePoints)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let nvc = segue.destination as! ViewController2
+        
+        nvc.secondStorePoints = storePoints
+    }
 }
 
